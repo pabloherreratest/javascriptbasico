fc1b12898bdadef2aac0d3ca0d972fe4d7f84807
Pablo Herrera <pabloherreratest@gmail.com>
2024-09-23 09:06:32 -0500

    operadores de comparación y lógicos

--- clases/persona03.js
+++ clases/persona03.js
@@ -2,18 +2,37 @@
 
 class Persona {
     
-    nombre = "Sin nombre"
-    paisNacimiento = "Sin país"
+    #nombre = "Sin nombre"
+    #paisNacimiento = "Sin país"
     
     saludar(){
-        console.log(`Hola, mi nombre es ${this.nombre} y soy del país ${this.paisNacimiento}.`);
+        console.log(`Hola, mi nombre es ${this.#nombre} y soy del país ${this.#paisNacimiento}.`);
     }
 
+    get nombre(){
+        return this.#nombre;
+    }
+
+    set nombre(valor){
+        if(valor === "")
+            console.log("El 'nombre' no puede ser vacío");
+        else
+            this.#nombre = valor;
+    }
+
+    get paisNacimiento(){
+        return this.#paisNacimiento;
+    }
+
+    set paisNacimiento(valor){
+        this.#paisNacimiento = valor;
+    }
 }
 
 let objPersona = new Persona();
 objPersona.nombre = "Pablo"
 objPersona.paisNacimiento = "Ecuador"
+console.log("nombre = ", objPersona.nombre);
 objPersona.saludar();
 
 objPersona = new Persona();
---
+++ condicional/operadores.js
@@ -0,0 +1,41 @@
+/* Operadores de comparación y lógicos */
+
+let x = 2;
+let y = 3;
+
+if (x > 2 || y === 3){
+    console.log(` "VERDAD"`);
+}
+else
+{
+    console.log(` "FALSO"`);
+}
+
+/*
+if (x !== "2"){
+    console.log(`x == ${x}, "VERDAD"`);
+}
+else
+{
+    console.log(`x == ${x}, "FALSO"`);
+}
+*/
+
+/*
+if (x > 2){
+    console.log(`x > ${x}, "VERDAD"`);
+}
+else
+{
+    console.log(`x > ${x}, "FALSO"`);
+}
+*/
+/*
+if (x !== 2){
+    console.log(`x == ${x}, "VERDAD"`);
+}
+else
+{
+    console.log(`x == ${x}, "FALSO"`);
+}
+    */
